e4c4d4dc076dab582a25e3ed564462bd27ce4082
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-05-16 13:58:12 -0400

    Added "fade-in-on-scroll"

---
+++ js/intersection-observer.js
@@ -0,0 +1,26 @@
+// Fade-in elements as they show on viewport
+// Reference: https://www.youtube.com/watch?v=huVJW23JHKQ
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    root: null, // This is the default
+    threshold: 1.00, // How much of the element has to be visible before firing? (as a percent, 0 --> 1)
+    rootMargin: "0px 0px -200px 0px"
+}
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.toggle('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions)
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
