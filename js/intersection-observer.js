69626aae6474a9949a2cd3acec6355efbf37fbed
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-05-21 13:02:08 -0400

    Added comments to explain how parts work

--- js/intersection-observer.js
+++ js/intersection-observer.js
@@ -1,25 +1,36 @@
 // Fade-in elements as they show on viewport
 // Reference: https://www.youtube.com/watch?v=huVJW23JHKQ
 
+
+// faders = "All elements with the .fade-in class"
 const faders = document.querySelectorAll('.fade-in');
 
+
+// These options are requried/used by "IntersectionObserver"
 const appearOptions = {
     root: null, // This is the default
     threshold: 0.3, // How much of the element has to be visible before firing? (as a percent, 0 --> 1)
-    rootMargin: "0px 0px -200px 0px"
+    rootMargin: "0px 0px -200px 0px" // This changes the size of the detectable box the screen has to intersect with
+                                     // Adding negative margin to the bottom (3rd value) is common, otherwise things 
+                                     // at the end of the page tend to do weird things.
+                                     // Essentially a negative value makes it show up easier.
 }
+
+
 
 const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
     entries.forEach(entry => {
-        if (!entry.isIntersecting) {
+        if (!entry.isIntersecting) {   //Everything starts out as not intersecting
             return;
-        } else {
-            entry.target.classList.toggle('appear');
-            appearOnScroll.unobserve(entry.target);
+        } else {    
+            entry.target.classList.toggle('appear');     // Toggle ('add') the "appear" class to the entry
+            appearOnScroll.unobserve(entry.target);      // Once it's appeared, there is no need to keep checking
         }
     })
-}, appearOptions)
+}, appearOptions) // Some options have to be passed in for the "IntersectionObserver". Defined above
 
+
+// This actually executes all of the stuff in place above. For each element with the "fade-in" class, we observe and appearonscroll
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 })
