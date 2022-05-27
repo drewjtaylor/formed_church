ad617c862f695bac38c0acab3980b462a1dc860c
Andrew Taylor <drew.j.taylor1@gmail.com>
2022-05-27 14:44:41 -0400

    Made elements appear easier with intersection observer

--- js/intersection-observer.js
+++ js/intersection-observer.js
@@ -9,13 +9,12 @@ const faders = document.querySelectorAll('.fade-in');
 // These options are requried/used by "IntersectionObserver"
 const appearOptions = {
     root: null, // This is the default
-    threshold: 0.3, // How much of the element has to be visible before firing? (as a percent, 0 --> 1)
+    threshold: 0.15, // How much of the element has to be visible before firing? (as a percent, 0.00 --> 1.00)
     rootMargin: "0px 0px -200px 0px" // This changes the size of the detectable box the screen has to intersect with
                                      // Adding negative margin to the bottom (3rd value) is common, otherwise things 
                                      // at the end of the page tend to do weird things.
                                      // Essentially a negative value makes it show up easier.
 }
-
 
 
 const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
